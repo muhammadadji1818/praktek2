98a08de2f0afc790027481580706530770309fbd
muhammadadji1818 <35833891+muhammadadji1818@users.noreply.github.com>
2018-04-02 20:02:19 +0800

    construnctor dengan parameter

--- src/praktek2/reactangelaksi.java
+++ src/praktek2/reactangelaksi.java
@@ -8,5 +8,12 @@ public class reactangelaksi {
          r1.cetakinfo();
          System.out.println("luas reactangel = "+r1.hitungluas());
          r1.cetakluas();
+         
+         reactangel r2 = new reactangel();
+         r2.cetakinfo();
+         
+         reactangel r3 = new reactangel(50,30);
+         r3.cetakinfo();
+         
     }
 }
