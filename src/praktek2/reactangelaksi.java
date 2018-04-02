8feeab0c90795ab6f02d7ab3ff854bd86f66dc61
muhammadadji1818 <35833891+muhammadadji1818@users.noreply.github.com>
2018-04-02 19:09:57 +0800

    mengisi nilai atrribut

--- src/praktek2/reactangelaksi.java
+++ src/praktek2/reactangelaksi.java
@@ -2,5 +2,7 @@ package praktek2;
 public class reactangelaksi {
     public static void main(String[] args) {
          reactangel r1 = new reactangel();
+         r1.panjang = 9;
+         r1.lebar = 3;
     }
 }
