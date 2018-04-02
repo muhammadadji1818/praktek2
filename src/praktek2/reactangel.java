276676daac4ace12f2703e53c70dc1897b2db254
muhammadadji1818 <35833891+muhammadadji1818@users.noreply.github.com>
2018-04-02 19:01:13 +0800

    membuat atrribut dari behavior

--- src/praktek2/reactangel.java
+++ src/praktek2/reactangel.java
@@ -1,4 +1,12 @@
 package praktek2;
 public class reactangel {
+    double panjang;
+    double lebar;
     
+    void cetakinfo(){
+        System.out.println("======================");
+        System.out.println("panjang : "+panjang");
+        System.out.println("lebar : "+lebar");
+        System.out.println("======================");
+    }
 }
